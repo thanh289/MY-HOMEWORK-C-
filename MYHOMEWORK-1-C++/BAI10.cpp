578877d8366b46403d64981b4c59311d4472b581
thanh289 <87253453+thanh289@users.noreply.github.com>
2021-08-17 09:52:59 +0700

    Create BAI10.cpp

---
+++ MYHOMEWORK-1-C++/BAI10.cpp
@@ -0,0 +1,47 @@
+//BAI 10
+#include <iostream>
+using namespace std;
+
+//ham sap xep bubble giam dan
+int bubblesort_giam(int a[], int n)
+{
+	for (int i=n-1;i>=0;i--)
+	{
+		for(int j=0;j<i;j++)
+		{
+			if (a[j]<a[j+1])
+				swap(a[j],a[j+1]);
+		}
+	}
+	
+	cout<<"mang sau khi sap xep la: "<<endl;
+	for (int i=0;i<n;i++)
+	{
+		cout<<a[i]<<" ";
+	}
+}
+
+
+
+//ham main
+int main()
+{
+	//khoi tao mang
+	int a[100];
+	
+	//khai bao n
+	int n;
+	cout<<"nhap so phan tu cua mang:"<<endl;
+	cin>>n;
+	
+	//nhap cac phan tu cua mang
+	cout<<"mang truoc khi sap xep la:"<<endl;
+	for(int i=0; i<n; i++)
+	{
+		cin>>a[i];
+	}	
+	
+	bubblesort_giam(a,n);
+	
+	return 0;
+}
