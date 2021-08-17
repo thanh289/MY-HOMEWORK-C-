b8a760824005dbaef527619bae1d4b5cdb5c6e6b
thanh289 <87253453+thanh289@users.noreply.github.com>
2021-08-17 09:53:07 +0700

    Create BAI11.cpp

---
+++ MYHOMEWORK-1-C++/BAI11.cpp
@@ -0,0 +1,48 @@
+//BAI 11
+#include <iostream>
+using namespace std;
+
+//ham sap xep insert giam dan
+int insertsort_giam(int a[], int n)
+{
+	for(int i=n-2; i>=0; i--)
+	{
+		int tempo =a[i];
+		int j=i+1;
+		while (j<=n-1 && tempo< a[j])
+		{
+			a[j-1]=a[j];
+			j=j+1;
+		}
+		a[j-1]=tempo;
+	}
+	
+	cout<<"mang sau khi sap xep la:"<<endl;
+	for(int i=0;i<n;i++)
+	{
+		cout<<a[i]<<" ";
+	}
+}
+
+//ham main
+int main()
+{
+	//khoi tao mang
+	int a[100];
+	
+	//khai bao n
+	int n;
+	cout<<"nhap so phan tu cua mang:"<<endl;
+	cin>>n;
+	
+	//nhap cac phan tu cua mang
+	cout<<"mang truoc khi sap xep la:"<<endl;
+	for(int i=0; i<n; i++)
+	{
+		cin>>a[i];
+	}	
+	
+	insertsort_giam(a,n);
+	
+	return 0;
+}
