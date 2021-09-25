53091889a6c55f009038b9c79d192c02c0dbf771
thanh289 <87253453+thanh289@users.noreply.github.com>
2021-09-25 22:40:59 +0700

    Update Phan 2-Bai3.cpp

--- MYHOMEWORK-4-C++/Phan 2-Bai3.cpp
+++ MYHOMEWORK-4-C++/Phan 2-Bai3.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <algorithm> //for sort()
+#include <vector>	//for vector 
 using namespace std;
 
 #define MAX 10000
+//trwocs khi xem cua sol
 
+/*
 int main()
 {
 	//khai bao mang
@@ -41,7 +44,7 @@ int main()
 		double ava, tempo=-10e9; //bien trung binh va bien tam
 		for(int i=0; i<n-1; i++)
 		{
-			sum1=sum1-a[i];  //tong cua f(a)
+			sum1-=a[i];  //tong cua f(a)
 			sum2+=a[i];		//trong cua f(b)
 
 			ava= (sum1/(n-i-1))+(sum2/(i+1)); //gia tri tb
@@ -53,4 +56,58 @@ int main()
 		
 		t--; //bien dem
 	}	
+}
+*/
+
+void solve()
+{
+	int n;
+	//nhap so phan tu cua day
+	cin>>n;
+	
+	//tao vector (co the dung mang 1 chieu)
+	vector <long long> A(n);
+	
+	//nhap cac phan tu vao day
+	for(int i=0; i<n; i++)
+		cin>>A[i];
+	
+	//sap xep lai day	
+	sort(A.begin(), A.end());
+	
+	//tao ra f(a) va f(b) theo de bai
+	long double sum1= 0, sum2=0;
+	//luu tong gia tri day vao f(b)
+	for(int i=0; i<n; i++)
+		sum2+= A[i];
+	
+	//gan vao bien  ket qua voi gia tri la gioi han yeu cau bai toan
+	long double ans=-1e18;
+	
+	for(int i=0; i<n-1; i++)
+	{
+		//lay tung phan tu cua f(b) sang f(a) den khi tim dc 
+		//gia tri lon nhat theo yeu cau bai toan
+		sum2 -= A[i];
+		sum1 += A[i];
+		
+		if(ans < sum1/(i+1) + sum2/(n-i-1))
+			ans = sum1/(i+1) + sum2/(n-i-1);
+	}
+	
+	//in ket qua ra man hinh
+	cout<<ans<<endl;
+}
+
+int main()
+{
+	int T=1;
+	//nhap so lan kiem tra
+	cin>>T;
+	while(T--)
+	{
+		//goi ham
+		solve();
+	}
+return 0;
 }
