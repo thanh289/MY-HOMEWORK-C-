192f72e78b88b7f152c1b859f097522d23f24ba0
thanh289 <87253453+thanh289@users.noreply.github.com>
2021-10-28 09:17:22 +0700

    Create BAI6.cpp

---
+++ MYHOMEWORK-1-C++(basic)/BAI6.cpp
@@ -0,0 +1,17 @@
+//BAI 6
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string str;
+	cout<<"Nhap chuoi can in ra man hinh"<<endl;
+	//getline cho lay khoang trang cua chuoi
+	getline(cin, str);
+	
+	cout<<"In ra man hinh: "<<endl;
+	cout<<str;
+	
+	return 0;
+}
