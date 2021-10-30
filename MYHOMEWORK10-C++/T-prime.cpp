a7b7e10739cc91df225da3a9d214c5002afd682d
thanh289 <87253453+thanh289@users.noreply.github.com>
2021-10-30 13:25:28 +0700

    Update T-prime.cpp

--- MYHOMEWORK10-C++/T-prime.cpp
+++ MYHOMEWORK10-C++/T-prime.cpp
@@ -1,37 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int k=10e6 +10;
-bool isPrime[k];
-
-int sieve(int x)
-{
-	for(int i=2; i<k; i++) isPrime[i]=true;
-	for(int i=2; i*i<k; i++)
-		if(isPrime[i]==true)
-			for(int j=i*i; j<k; j+=i) isPrime[j]=false;
-			
-//	vector <int> p;
-//	for(int i=2; i<=k; i++)
-//		if(isPrime[i]==true) p.push_back(i);
-	
-	int check;
-	if(isPrime[x]==true) check=1;
-	else check=0;
-	return check;
-}
-
+int isPrime[k];
 
 int main()
 {
-    int t;cin>>t;
-    for(int i=1; i<=t; i++)
+	isPrime[0]=1;
+	isPrime[1]=1;
+	for(int i=2; i*i<k; i++)
+		if(!isPrime[i])
+			for(int j=i*i; j<k; j+=i) isPrime[j]=1;
+			
+
+	
+	int t;cin>>t;
+    while(t--)
     {
         long long x; cin>>x;
         long long y=sqrt(x);
-        if(y*y==x && sieve(y))
+        if(y*y==x && !isPrime[y])
             cout<<"YES"<<"\n";
         else
             cout<<"NO"<<"\n";
     }
-    return 0;
+
 }
+
