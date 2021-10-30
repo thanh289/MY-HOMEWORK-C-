e5dac03bf2b1b941fd44494ebb8b45491c378267
thanh289 <87253453+thanh289@users.noreply.github.com>
2021-10-30 13:25:56 +0700

    Create Prime Subtraction.cpp

---
+++ MYHOMEWORK10-C++/Prime Subtraction.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll; 
+
+//vi cac so luon co the phan tich thanh tich cac so nguyen to nen luon ton tai so nguyen to a ma x-y%a==0
+int main()
+{
+	int t; cin>>t;
+	while(t--)
+	{
+	ll x,y; cin>>x>>y;
+	if(x-y<=1) cout<<"NO"<<endl;
+	else cout<<"YES"<<endl;
+	}
+}
