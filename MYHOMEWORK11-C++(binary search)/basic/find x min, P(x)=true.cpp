a7296290ea30be9f899e7dbdb9e0e71ead2a489d
thanh289 <87253453+thanh289@users.noreply.github.com>
2021-11-04 22:49:36 +0700

    Create find x min, P(x)=true.cpp

---
+++ MYHOMEWORK11-C++(binary search)/basic/find x min, P(x)=true.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool P(int a[], int x, int target)
+{
+	if(a[x] >= target)
+		return true;
+	return false;
+}
+
+int binary_search(int lo, int hi, int target, int a[])
+{
+	while(lo<hi)
+	{
+		int mid= (lo+hi)/2;
+		if(P(a, mid, target) == true)
+			hi=mid;
+		else
+			lo=mid+1;
+	}
+	
+	if(P(a, lo, target) == false)
+		return -1;
+		
+	return lo;
+}
+
+int main()
+{
+	int n=20, k=3;
+	int a[]={1,1,1,1,2,2,3,3,3,3,3,3,5,5,5,5,5,6,6,7};
+	cout<<"vi tri khoa: "<<binary_search(0, n-1, k, a);
+}
