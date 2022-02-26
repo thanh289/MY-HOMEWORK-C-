0133799f45954e3a73023e89ad79cb0b12316373
thanh289 <87253453+thanh289@users.noreply.github.com>
2022-02-26 21:28:00 +0700

    Update Prime Square.cpp

--- MYHOMEWORK10-C++/Prime Square.cpp
+++ MYHOMEWORK10-C++/Prime Square.cpp
@@ -1,34 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-	int t; cin>>t;
-	while(t--)
-	{
-		int n; cin>>n;
-		int prime[n][n];
-		for(int i=0; i<n; i++)
-			for(int j=0; j<n; j++) prime[i][j]=0;
+int a[101][101];
 
-		for(int i=0; i<n; i++)
-		{
-			prime[i][n-i-1]=1;
-			prime[i][i]=1;
-		}
-		
-		if(n%2==1)
-		{
-			prime[n/2][0]=1;
-			prime[n/2][n-1]=1;
-			prime[0][n/2]=1;
-			prime[n-1][n/2]=1;
-		}
-		
-		for(int i=0; i<n; i++)
-		{
-			for(int j=0; j<n; j++) cout<<prime[i][j]<<" ";
-			cout<<endl;
+//c1: map look like: 
+//1 0 0 0 0 1
+//0 1 0 0 1 0
+//0 0 1 1 0 0
+//0 0 1 1 0 0
+//0 1 0 0 1 0
+//1 0 0 0 0 1
+
+//int main(){
+//	int t;  cin>>t;
+//	while(t--){
+//		int n; cin>>n;
+//		for(int i=0; i<n; i++)
+//			for(int j=0; j<n; j++) a[i][j]=0;
+//			
+//		for(int i=0; i<n; i++){
+//			a[i][i] =1;
+//			a[i][n-i-1]=1;
+//		}
+//		
+//		if(n%2==1){
+//			a[0][n/2]=1;
+//			a[n/2][0]=1;
+//			a[n-1][n/2]=1;
+//			a[n/2][n-1]=1;
+//		}
+//		
+//		for(int i=0; i<n; i++){
+//			for(int j=0; j<n; j++){
+//				cout<<a[i][j]<<" ";
+//			}
+//			cout<<endl;
+//		}	
+//	}
+//}
+
+
+
+//c2: map look like: 
+//(1 1) 0 0 0 0
+//0 (1 1) 0 0 0
+//0 0 (1 1) 0 0
+//0 0 0 (1 1) 0
+//0 0 0 0 (1 1)
+//1) 0 0 0 0 (1
+
+int main(){
+	int t; cin>>t;
+		while(t--){
+		int n; cin>>n;
+		for(int i=0; i<n; i++){
+			for(int j=0; j<n; j++){
+				cout<<(i==j || i==j-1 || (i==n-1 && j==0))<<(j==n-1 ? "\n" : " ");
+			}
 		}
 	}
 }
+
