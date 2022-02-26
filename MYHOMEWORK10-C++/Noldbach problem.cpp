56bb7a3e343671e41b9b51c1ddf064ea56a2a923
thanh289 <87253453+thanh289@users.noreply.github.com>
2022-02-26 21:28:09 +0700

    Update Noldbach problem.cpp

--- MYHOMEWORK10-C++/Noldbach problem.cpp
+++ MYHOMEWORK10-C++/Noldbach problem.cpp
@@ -1,26 +1,31 @@
+
 #include <bits/stdc++.h>
 using namespace std;
-const int m=1001;
-int isPrime[m];
-int main()
-{	
-	for(int i=2; i*i<m; i++)
-		if(!isPrime[i])
-			for(int j=i*i; j<m; j+=i) isPrime[j]=1;
-			
-	vector <int> v;	
-	for(int i=2; i<m; i++)
-		if(!isPrime[i]) v.push_back(i);
-	
-	int n, k, count=0;; cin>>n>>k;
-	
-	for(int i=0; v[i]<n; i++)
-	{
-		int x=v[i]+v[i+1]+1;
-		if(!isPrime[x] && x<=n ) count++;
-		if(x>n) break;
+
+#define N 1010
+int isPrime[N];
+
+int main(){
+	for(int i=2; i*i<N; i++){
+		if(!isPrime[i]){
+			for(int j=i*i; j<N; j+=i)
+				isPrime[j] = 1;		
+		}
 	}
+	vector<int> v;
+	for(int i=2; i<N; i++) 
+		if(!isPrime[i])
+			v.push_back(i);
 	
-	if(count>=k) cout<<"YES"<<endl;
+	int n, k, ans=0;  cin>>n>>k;
+	for(int i=0; i<v.size()-1; i++){
+		int x = v[i]+v[i+1]+1;
+		if(!isPrime[x] && x<=n) ans++;
+		else if(x>n) break;
+	}
+	if(ans>= k) cout<<"YES"<<endl;
 	else cout<<"NO"<<endl;
 }
+
+
+
