cdab7e6390102898837e34704b6e1419b908b128
thanh289 <87253453+thanh289@users.noreply.github.com>
2022-02-26 21:28:27 +0700

    Update Almost Prime.cpp

--- MYHOMEWORK10-C++/Almost Prime.cpp
+++ MYHOMEWORK10-C++/Almost Prime.cpp
@@ -1,54 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-//sang nguyen to
-vector <int> seive()
-{
-	bool isPrime[3001];
-	for(int i=2; i<=3000; i++)
-	{
-		isPrime[i] = true;
-	}
-	
-	
-	for(int i=2; i*i<= 3000; i++)
-	{
-		if(isPrime[i] == true)
-		{
-			for(int j=i*i; j<=3000; j+=i)
-			{
-				isPrime[j] = false;
-			}
+ 
+#define N 3010
+int isPrime[N];
+int ans = 0;
+int main(){
+	int n;  cin>>n;
+	for(int i=2; i*i<=n; i++){
+		if(!isPrime[i]){
+			for(int j=i*i; j<=n; j+=i)
+				isPrime[j] = 1;
 		}
 	}
 	
-	vector <int> p;
-	for(int i=2; i<=3000; i++)
-		if(isPrime[i]==true) p.push_back(i);
-		
-	return p;
-}
-
-
-void solve(int n)
-{
-	vector <int> v=seive();
-	int count=0;
-	for(int i=1; i<=n; i++)
-	{
-		int divisor=0;
-		for(int j=0; j<v.size(); j++)
-		{
+	vector <int> v;
+	for(int i=2; i<=n; i++){
+		if(!isPrime[i]) v.push_back(i);
+	}
+	
+//	for(int i=0; i<v.size(); i++) cout<<v[i]<<endl;	
+	for(int i=6; i<=n; i++){
+		int divisor = 0;
+		for(int j=0; j<v.size(); j++){
+			if(i<=v[j]) break;
 			if(i%v[j]==0) divisor++;
-			if(v[j]>i) break;
 		}
-		if (divisor==2) count++;
+		
+		if(divisor==2) ans++;
 	}
-	cout<<count<<endl;
+	cout<<ans;
 }
-
-int main()
-{
-	int n; cin>>n;
-	solve(n);
-}
+ 
